e68f065f2786f5856e22f37d36f40362371ac656
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-02-23 22:02:24 -0800

    Finish basic stack

--- linear-structures/stack.js
+++ linear-structures/stack.js
@@ -12,6 +12,38 @@ class Stack {
   size() {
     return _size.get(this);
   }
+
+  clear() {
+    _data.set(this, []);
+    _size.set(this, 0);
+    return this;
+  }
+
+  push(value) {
+    let size = _size.get(this);
+    _size.set(this, size + 1);
+    return _data.get(this).push(value);
+  }
+
+  pop() {
+    let size = _size.get(this);
+    if (size > 0) {
+      _size.set(this, size - 1);
+    } 
+    return _data.get(this).pop();
+  }
+
+  // top value is returned but not removed
+  peek() {
+    let data = _data.get(this);
+    return data[data.length - 1];
+  }
+
+  reverse() {
+    let data = _data.get(this);
+    _data.set(this, data.reverse());
+    return this;
+  }
 }
 
 module.exports = Stack;
--- linear-structures/test/testStack.js
+++ linear-structures/test/testStack.js
@@ -10,6 +10,59 @@ describe('Stack', () => {
     it('should create an empty stack', () => {
       let stack = new Stack();
       expect(stack.size()).to.equal(0);
+      expect(stack.pop()).to.be(undefined);
+    });
+  });
+
+  describe('clear', () => {
+    it('should empty the stack', () => {
+      let stack = new Stack();
+      stack.push(1);
+      stack.clear();
+      expect(stack.size()).to.equal(0);
+      expect(stack.pop()).to.be(undefined);
+    });
+  });
+
+  describe('push & pop', () => {
+    let stack = new Stack();
+    it('should push new elements onto the stack', () => {
+      stack.push('a');
+      expect(stack.size()).to.equal(1);
+      stack.push('b');
+      expect(stack.size()).to.equal(2);
+    });
+    it('should retrieve those elements by popping', () => {
+      expect(stack.pop()).to.equal('b');
+      expect(stack.size()).to.equal(1);
+      expect(stack.pop()).to.equal('a');
+      expect(stack.size()).to.equal(0);
+      expect(stack.pop()).to.be(undefined);
+      expect(stack.size()).to.equal(0);
+    });
+  });
+
+  describe('peek', () => {
+    it('should return the top value without removing it', () => {
+      let stack = new Stack();
+      stack.push('a');
+      stack.push('b');
+      expect(stack.peek()).to.equal('b');
+      expect(stack.size()).to.equal(2);
+    });
+  });
+
+  describe('reverse', () => {
+    it('should reverse the stack', () => {
+      let stack = new Stack();
+      stack.push('a');
+      stack.push('b');
+      stack.push('c');
+      stack.reverse();
+      expect(stack.size()).to.equal(3);
+      expect(stack.pop()).to.equal('a');
+      expect(stack.pop()).to.equal('b');
+      expect(stack.pop()).to.equal('c');
     });
   });
 
