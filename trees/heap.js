7008b01917220c4b8f2073a79aa8b3e3d91cfed4
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-02-26 23:54:12 -1000

    Switch to unprotected data field

--- trees/heap.js
+++ trees/heap.js
@@ -1,14 +1,47 @@
 'use strict';
 
-let _data = new WeakMap();
-
 class Heap {
   constructor() {
-    _data.set(this, []);
+    this._data = [];
   }
 
   size() {
-    return _data.get(this).length;
+    return this._data.length;
+  }
+
+  _leftChild(i) {
+    return 2 * i + 1;
+  }
+
+  _rightChild(i) {
+    return 2 * i + 2;
+  }
+
+  _swap(i, j) {
+    let temp = this._data[i];
+    this._data[i] = this._data[j];
+    this._data[j] = temp;
+  }
+
+  _percolateUp() {
+
+  }
+
+  push(value) {
+
+  }
+
+  pop(value) {
+
+  }
+
+  static heapify(arr) {
+    if (!Array.isArray(arr)) {
+      // convert an iterable into an array
+      arr = [...arr];
+    }
+    let heap = new Heap();
+    return arr;           // NOT FINISHED!!!
   }
 }
 
