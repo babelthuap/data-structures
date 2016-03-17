23cc81e83ba7319d4c7450f169d2abd21fe91a86
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-03-17 18:18:06 -0500

    Finish heap implementation

--- trees/heap.js
+++ trees/heap.js
@@ -17,22 +17,73 @@ class Heap {
     return 2 * i + 2;
   }
 
+  _parent(i) {
+    return Math.floor((i - 1) / 2);
+  }
+
   _swap(i, j) {
     let temp = this._data[i];
     this._data[i] = this._data[j];
     this._data[j] = temp;
   }
 
-  _percolateUp() {
+  // recursively swap with parent until parent is less than or equal to the
+  // current element
+  _percolateUp(i) {
+    if (i === 0) {
+      return;
+    }
+    let parent = this._parent(i);
+    if (this._data[parent] <= this._data[i]) {
+      return;
+    } else {
+      this._swap(i, parent);
+      this._percolateUp(parent);
+    }
+  }
 
+  // recursively swap with smaller child until both children are greater than or
+  // equal to the current element
+  _percolateDown(i) {
+    let left = this._leftChild(i);
+    if (left >= this._data.length) {
+      return;
+    }
+    let right = this._rightChild(i);
+    if (this._data[left] > this._data[right]) {
+      if (this._data[right] < this._data[i]) {
+        // swap with right child
+        this._swap(i, right);
+        this._percolateDown(right);
+      }
+    } else {
+      // right child must be larger or undefined
+      if (this._data[left] < this._data[i]) {
+        // swap with left child
+        this._swap(i, left);
+        this._percolateDown(left);
+      }
+    }
   }
 
   push(value) {
-
+    this._data.push(value);
+    this._percolateUp(this._data.length - 1);
   }
 
   pop(value) {
+    let min = this._data[0];
+    if (this._data.length > 1) {
+      this._data[0] = this._data.pop();
+      this._percolateDown(0);
+    } else {
+      this._data.pop();
+    }
+    return min;
+  }
 
+  peek() {
+    return this._data[0];
   }
 
   static heapify(arr) {
@@ -41,7 +92,22 @@ class Heap {
       arr = [...arr];
     }
     let heap = new Heap();
-    return arr;           // NOT FINISHED!!!
+    arr.forEach(element => heap.push(element));
+    return heap;
+  }
+
+  static heapSort(arr) {
+    let heap = this.heapify(arr);
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = heap.pop();
+    }
+    return arr;
+  }
+
+  print() {
+    for (let lvl = 0; lvl < this._data.length; lvl = 2 * lvl + 1) {
+      console.log( this._data.slice(lvl, 2 * lvl + 1).join(' ') );
+    }
   }
 }
 
--- trees/test/testHeap.js
+++ trees/test/testHeap.js
@@ -13,4 +13,42 @@ describe('Heap', () => {
     });
   });
 
+  describe('push', () => {
+    it('should add an element and maintain the heap structure', () => {
+      let heap = new Heap();
+      heap.push(3);
+      heap.push(2);
+      heap.push(1);
+      expect(heap._data).to.eql([1, 3, 2]);
+    });
+  });
+
+  describe('heapify', () => {
+    it('should turn an array into a heap', () => {
+      let heap = Heap.heapify([3, 7, 0, 0, 6, 5, 1]);
+      expect(heap._data).to.eql([0, 0, 1, 7, 6, 5, 3]);
+    });
+  });
+
+  describe('pop', () => {
+    it('should pull out the min from the heap', () => {
+      let heap = Heap.heapify([3, 7, 0, 0, 6, 5, 1]);
+      expect(heap.pop()).to.equal(0);
+      expect(heap.pop()).to.equal(0);
+      expect(heap.pop()).to.equal(1);
+      expect(heap.pop()).to.equal(3);
+      expect(heap.pop()).to.equal(5);
+      expect(heap.pop()).to.equal(6);
+      expect(heap.pop()).to.equal(7);
+      expect(heap.pop()).to.be(undefined);
+    });
+  });
+
+  describe('heapSort', () => {
+    it('should sort an array', () => {
+      let arr = Heap.heapSort([2, 7, 4, 27, -3, -100, 2, 2, 5, 7, 8, 7, 1]);
+      expect(arr).to.eql([-100, -3, 1, 2, 2, 2, 4, 5, 7, 7, 7, 8, 27]);
+    });
+  });
+
 });
